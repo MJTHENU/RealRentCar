fa609780105929dca9878bebd9bb6562f9cee1f0
Thenmozhi <cthenmozhi96@gmail.com>
2024-04-12 13:41:48 +0530

    Login API

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\Auth\LoginController;
 
 // User registration route
 Route::post('/register', [RegisterController::class, 'register']);
+
+// User login route
+Route::post('login', [LoginController::class, 'login']);
 
 // Other API routes can be defined here
